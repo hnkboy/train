b1a74f40a1e7e2e4c5046007f0780bef2193111c
hnkboy <709256704@qq.com>
2020-10-19 11:16:36 +0800

    dpdk

--- modules/dpdk/test.c
+++ modules/dpdk/test.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <sched.h>
+#include <time.h>
+#include <rte_config.h>
+#include <rte_compat.h>
+#include <rte_per_lcore.h>
+#include <rte_bus.h>
+#include <rte_uuid.h>
+#include <rte_pci_dev_feature_defs.h>
+//参考，dpdk 的examples
 int main(int argc, char *argv[])
 {
     int ret;
